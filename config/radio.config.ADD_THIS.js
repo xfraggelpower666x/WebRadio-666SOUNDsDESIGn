cc6a1cf9dcae1e3a7872a5f61178abb4bb081ab5
fraggelpower666 <fraggelpower666@googlemail.com>
2026-04-08 21:34:05 +0200

    Add files via upload

---
+++ config/radio.config.ADD_THIS.js
@@ -0,0 +1,7 @@
+// ADD THIS TO YOUR EXISTING CONFIG
+
+soundcloudFallbackSources: [
+  "https://soundcloud.com/DEIN-ALBUM-1",
+  "https://soundcloud.com/DEIN-ALBUM-2",
+  "https://soundcloud.com/DEIN-ALBUM-3"
+],
