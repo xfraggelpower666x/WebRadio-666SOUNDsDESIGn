cef69c79c0c848f560420b65cf6323335287e8c3
fraggelpower666 <fraggelpower666@googlemail.com>
2026-04-09 03:05:50 +0200

    upload js/device-mode.js

--- js/device-mode.js
+++ js/device-mode.js
@@ -1,12 +1,1 @@
-window.DeviceMode = (() => {
-  const ua = navigator.userAgent || "";
-  const isIPhone = /iPhone/i.test(ua);
-  const isIPad = /iPad/i.test(ua) || (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
-  const isAndroid = /Android/i.test(ua);
-  const mode = isIPhone || isIPad ? "iphone" : isAndroid ? "android" : "desktop";
-  return {
-    mode,
-    apply(){ document.documentElement.setAttribute("data-device-mode", mode); },
-    stallMs(){ return mode === "iphone" ? 22000 : mode === "android" ? 12000 : 8000; }
-  };
-})();
+window.DeviceMode=(()=>{const ua=navigator.userAgent||"",isIPhone=/iPhone/i.test(ua),isIPad=/iPad/i.test(ua)||(navigator.platform==="MacIntel"&&navigator.maxTouchPoints>1),isAndroid=/Android/i.test(ua),mode=isIPhone||isIPad?"iphone":isAndroid?"android":"desktop";return{mode,apply(){document.documentElement.setAttribute("data-device-mode",mode)},stallMs(){return mode==="iphone"?22000:mode==="android"?12000:8000}}})();
