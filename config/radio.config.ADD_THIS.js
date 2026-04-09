e93a13dde93a41b265bc7e327326816fc6c629b5
fraggelpower666 <fraggelpower666@googlemail.com>
2026-04-09 13:07:21 +0200

    Delete config directory

--- config/radio.config.ADD_THIS.js
+++
@@ -1,7 +0,0 @@
-// ADD THIS TO YOUR EXISTING CONFIG
-
-soundcloudFallbackSources: [
-  "https://soundcloud.com/DEIN-ALBUM-1",
-  "https://soundcloud.com/DEIN-ALBUM-2",
-  "https://soundcloud.com/DEIN-ALBUM-3"
-],
--- config/radio.config.js
+++
@@ -1,7 +0,0 @@
-
-window.RADIO_CONFIG = {
-  apiBase: "https://webradio-666soundsdesign-worker.fragglepower666.workers.dev",
-  streamPath: "/api/radio/stream",
-  metadataPath: "/api/radio/metadata",
-  healthPath: "/health"
-};
