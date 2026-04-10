8ded134acfe3c41482889645a2c1d31f51e157d5
fraggelpower666 <fraggelpower666@googlemail.com>
2026-04-10 15:23:04 +0200

    Delete js directory

--- js/mixcloud-panel.js
+++
@@ -1,9 +0,0 @@
-window.MixcloudPanel = {
-  init() {
-    const frame = document.getElementById("mixFrame");
-    if (!frame || !window.RADIO_CONFIG) return;
-    frame.src =
-      "https://www.mixcloud.com/widget/iframe/?feed=" +
-      encodeURIComponent(RADIO_CONFIG.mixcloudFeed);
-  }
-};
