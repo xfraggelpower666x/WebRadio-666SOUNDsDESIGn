779cba4d28327b74b4dcd4cd3dc811f1cb369c95
fraggelpower666 <fraggelpower666@googlemail.com>
2026-04-14 19:11:52 +0200

    upload config/stream.config.js

--- config/stream.config.js
+++ config/stream.config.js
@@ -1,8 +1,9 @@
 export const STREAM_CONFIG = {
   "primary_stream_url": "https://my.idjstream.com/666soundsdesign/stream",
   "fallback_stream_url": "https://my.idjstream.com/8686/stream",
-  "api_url": "https://my.idjstream.com/cp/get_info.php?p=8686",
+  "api_url": "/api/nowplaying",
+  "upstream_api_url": "https://my.idjstream.com/cp/get_info.php?p=8686",
   "poll_interval_ms": 10000,
-  "stream_timeout_ms": 9000,
+  "listener_capacity": 250,
   "use_webhook": false
 };
