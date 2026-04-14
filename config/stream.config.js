46c64107ed057fa23a938dc18a0e182f036a6d4f
fraggelpower666 <fraggelpower666@googlemail.com>
2026-04-14 20:03:21 +0200

    upload config/stream.config.js

--- config/stream.config.js
+++ config/stream.config.js
@@ -1,9 +1,6 @@
 export const STREAM_CONFIG = {
-  "primary_stream_url": "https://my.idjstream.com/666soundsdesign/stream",
+  "primary_stream_url": "https://666soundsdesign-broadcaster.com/stream",
   "fallback_stream_url": "https://my.idjstream.com/8686/stream",
-  "api_url": "/api/nowplaying",
-  "upstream_api_url": "https://my.idjstream.com/cp/get_info.php?p=8686",
-  "poll_interval_ms": 10000,
-  "listener_capacity": 250,
+  "poll_interval_ms": 0,
   "use_webhook": false
 };
