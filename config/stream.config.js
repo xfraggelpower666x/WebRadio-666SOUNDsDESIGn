5e80f6ce98c0596ab906cac8614c463c55c6a133
fraggelpower666 <fraggelpower666@googlemail.com>
2026-04-14 22:15:47 +0200

    upload config/stream.config.js

--- config/stream.config.js
+++ config/stream.config.js
@@ -1,7 +1,7 @@
 export const STREAM_CONFIG = {
   "stream_url": "/stream",
   "fallback_stream_url": "/fallback-stream",
-  "metadata_url": "/api/nowplaying",
+  "metadata_url": "/metadata",
   "poll_interval_ms": 8000,
   "listener_capacity": 250,
   "use_webhook": false,
