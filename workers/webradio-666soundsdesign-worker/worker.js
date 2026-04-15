5b7fc91a2d6be0a6697ef39b026723bb85328b1a
fraggelpower666 <fraggelpower666@googlemail.com>
2026-04-15 16:56:59 +0200

    upload workers/webradio-666soundsdesign-worker/worker.js

--- workers/webradio-666soundsdesign-worker/worker.js
+++ workers/webradio-666soundsdesign-worker/worker.js
@@ -1,25 +1,20 @@
+// SAME WORKER IN BOTH LOCATIONS
+const PRIMARY_STREAM_URL = "https://my.idjstream.com/666soundsdesign/stream";
+
 export default {
   async fetch(request) {
     const url = new URL(request.url);
 
     if (url.pathname === "/stream") {
-      return fetch("https://my.idjstream.com/666soundsdesign/stream");
+      const res = await fetch(PRIMARY_STREAM_URL);
+      return new Response(res.body, {
+        headers: {
+          "content-type": "audio/mpeg",
+          "access-control-allow-origin": "*"
+        }
+      });
     }
 
-    return new Response(`<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>666SOUNDsDESIGn Radio</title>
-</head>
-<body style="background:#111;color:#fff;text-align:center;padding:40px;">
-<h1>666SOUNDsDESIGn Radio</h1>
-<audio controls autoplay style="width:80%;">
-  <source src="/stream" type="audio/mpeg">
-</audio>
-</body>
-</html>`, {
-      headers: { "content-type": "text/html" }
-    });
+    return new Response("Worker OK");
   }
 };
