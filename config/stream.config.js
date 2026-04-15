0f951b02aa3d397fbaa71eff784bde8e51eaf54e
fraggelpower666 <fraggelpower666@googlemail.com>
2026-04-15 16:13:49 +0200

    upload config/stream.config.js

--- config/stream.config.js
+++ config/stream.config.js
@@ -1,7 +1,7 @@
 export const STREAM_CONFIG = {
-  stream_url: "https://webradio-666soundsdesign-worker.digital-underground-connected.workers.dev/stream",
-  fallback_stream_url: "https://webradio-666soundsdesign-worker.digital-underground-connected.workers.dev/fallback-stream",
-  metadata_url: "https://webradio-666soundsdesign-worker.digital-underground-connected.workers.dev/api/nowplaying",
+  stream_url: "/stream",
+  fallback_stream_url: "/fallback-stream",
+  metadata_url: "/api/nowplaying",
   poll_interval_ms: 8000,
   listener_capacity: 250,
   use_webhook: false
