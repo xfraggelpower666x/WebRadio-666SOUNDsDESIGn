4a45ec6c985631adb95f2ea8e8c3565ca9efe182
fraggelpower666 <fraggelpower666@googlemail.com>
2026-04-16 13:38:34 +0200

    UPLOAD config/stream.config.js

--- config/stream.config.js
+++ config/stream.config.js
@@ -1,10 +1,10 @@
-export const STREAM_CONFIG = {
-  stream_url: "https://webradio.666soundsdesign-broadcaster.com/stream",
-  fallback_stream_url: "https://webradio.666soundsdesign-broadcaster.com/fallback-stream",
-  metadata_url: "https://webradio.666soundsdesign-broadcaster.com/api/nowplaying",
-  poll_interval_ms: 8000,
-  listener_capacity: 250,
-  use_webhook: false,
-  primary_upstream: "https://my.idjstream.com/666soundsdesign/stream",
-  fallback_upstream: "https://my.idjstream.com:8686/stream"
+window.STREAM_CONFIG = {
+  "stream_url": "/stream",
+  "fallback_stream_url": "/fallback-stream",
+  "metadata_url": "/api/nowplaying",
+  "poll_interval_ms": 8000,
+  "listener_capacity": 250,
+  "use_webhook": false,
+  "primary_upstream": "https://my.idjstream.com/666soundsdesign/stream",
+  "fallback_upstream": "https://my.idjstream.com:8686/stream"
 };
